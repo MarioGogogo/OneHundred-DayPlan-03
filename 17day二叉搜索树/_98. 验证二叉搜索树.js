fca45d9a8890e5798fe72bbfcbd5a91bd09c7c20
EdisonChan <148373644@qq.com>
2022-05-05 21:40:24 +0800

    update:二叉搜索树

--- 17day二叉搜索树/_98. 验证二叉搜索树.js
+++ 17day二叉搜索树/_98. 验证二叉搜索树.js
@@ -19,7 +19,7 @@
 const helper = (root, lower, upper) => {
   //边界条件
   if (root === null) return true;
-  //边界条件
+  //满足 二叉搜索 条件 节点的左子树只包含 小于 当前节点的数。节点的右子树只包含 大于 当前节点的数。
   if (root.val <= lower || root.val >= upper) return false;
   const left = helper(root.left, lower, root.val);
   const right = helper(root.right, root.val, upper);
